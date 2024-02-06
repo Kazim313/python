39e0719fdcbbaf5d2656653a2af5fc050f57ec2c
Kazim313 <kazimalichanna@gmail.com>
2024-02-06 13:36:23 +0500

    getting inupt from user

---
+++ input.py
@@ -0,0 +1,13 @@
+# now we will get input from user
+
+fname = input("input first name = ")
+lname = input("input last name = ")
+age = input("input you age = ")
+print("my good name is "+fname,lname)
+print("and my age is = "+age+" years")
+
+"""
+old_age = input("enter your old age ")
+
+new_age = int(old_age) + 2
+print(new_age)"""
