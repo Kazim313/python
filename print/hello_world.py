1bb2a46d64b5e8da5ce9a4b623b7e94e67387df0
Kazim313 <kazimalichanna@gmail.com>
2024-02-06 13:24:23 +0500

    print statements

--- print/hello_world.py
+++ print/hello_world.py
@@ -4,12 +4,3 @@ name= "Ali"
 age=12
 gender="male"
 print(name,age,gender)
-
-# now we will get input from user
-
-name1 = input("input name ")
-print(name1)
-old_age = input("enter your old age ")
-
-new_age = int(old_age) + 2
-print(new_age)
